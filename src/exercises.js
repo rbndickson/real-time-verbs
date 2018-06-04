6af2162505c3bdcf5ff85c7c20222e466766b7ac
Robin Dickson <robindickson@gmail.com>
2018-06-04 21:36:38 +0700

    feat: Add verb exercise data

---
+++ src/exercises.js
@@ -0,0 +1,10 @@
+export const exercises = {
+  past: {
+    instruction: "Make a past sentence.",
+    examples: [
+      "He / play / O => He played soccer.",
+      "I / play / X => I didn't play tennis.",
+      "They / play / ? => Did they play baseball?"
+    ]
+  }
+};
