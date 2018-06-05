c72de0d23f8c0bea3ef53373b613081a08d33b28
Robin Dickson <robindickson@gmail.com>
2018-06-05 18:57:15 +0700

    feat: Add generateToken helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,11 @@
 export const sample = array => array[Math.floor(Math.random() * array.length)];
+
+export const generateToken = tokenLength => {
+  var token = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < tokenLength; i++)
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return token;
+};
