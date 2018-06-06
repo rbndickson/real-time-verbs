68d2de6e4d42d69e0baa74e5dd3adcb9db09204c
Robin Dickson <robindickson@gmail.com>
2018-06-07 00:54:18 +0700

    feat: Fix position of question words

--- src/Question.js
+++ src/Question.js
@@ -2,11 +2,15 @@ import React from "react";
 
 const Question = props => (
   <div className="tc mt5 mb5">
-    {props.question.map(e => (
-      <span key={e} className="f1 mr4 ml4 pa3 bg-light-pink">
-        {e}
-      </span>
-    ))}
+    <div key={props.question[0]} className="dib w-20 f1 mh3 pv2 bg-light-pink">
+      {props.question[0]}
+    </div>
+    <div key={props.question[1]} className="dib w-30 f1 mh3 pv2 bg-light-pink">
+      {props.question[1]}
+    </div>
+    <div key={props.question[2]} className="dib w-10 f1 mh3 pv2 bg-light-pink">
+      {props.question[2]}
+    </div>
   </div>
 );
 
