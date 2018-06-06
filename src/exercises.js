792062ac7343135463a99d66c8a2fad5dd12d080
Robin Dickson <robindickson@gmail.com>
2018-06-07 01:29:56 +0700

    feat: Add omitted words for past tense exercise

--- src/exercises.js
+++ src/exercises.js
@@ -2,18 +2,19 @@ export const exercises = {
   past: {
     instruction: "Make a past sentence.",
     examples: [
-      {
-        question: ["He", "play", "O"],
-        solution: "He played soccer."
-      },
-      {
-        question: ["I", "play", "X"],
-        solution: "I didn't play tennis."
-      },
-      {
-        question: ["They", "play", "?"],
-        solution: "Did they play baseball?"
-      }
+      { question: ["He", "play", "O"], solution: "He played soccer." },
+      { question: ["I", "play", "X"], solution: "I didn't play tennis." },
+      { question: ["They", "play", "?"], solution: "Did they play baseball?" }
+    ],
+    omitVerbs: [
+      "will",
+      "would",
+      "could",
+      "may",
+      "should",
+      "must",
+      "might",
+      "going"
     ]
   }
 };
