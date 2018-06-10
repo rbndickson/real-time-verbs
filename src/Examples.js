8c76f36ecd94a8625bbea70e304c5b69bb51e50c
Robin Dickson <robindickson@gmail.com>
2018-06-10 19:54:02 +0700

    refactor: Use TableData component

--- src/Examples.js
+++ src/Examples.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const TableData = props => <td className="pr4">{props.data}</td>;
+
 const Examples = props => (
   <article className="mw5 mw6-ns br3 hidden ba b--black-10 mv4">
     <h4 className="f5 bg-near-white br3 br--top black-60 mv0 pv2 ph3">
@@ -10,16 +12,10 @@ const Examples = props => (
         <tbody>
           {props.examples.map((example, i) => (
             <tr key={example.question.join("")}>
-              <td className="pr4" key={i}>
-                {i + 1}.
-              </td>
-              {example.question.map(e => (
-                <td className="pr4" key={e}>
-                  {e}
-                </td>
-              ))}
-              <td className="pr4">=></td>
-              <td>{example.solution}</td>
+              <TableData key={i} data={`${i + 1}.`} />
+              {example.question.map(e => <TableData key={e} data={e} />)}
+              <TableData data="=>" />
+              <TableData data={example.solution} />
             </tr>
           ))}
         </tbody>
