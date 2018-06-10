2e83025c227e721596d7e53027a94e2ccfb5f3c1
Robin Dickson <robindickson@gmail.com>
2018-06-10 15:28:59 +0700

    refactor: Make highlighting a reusable component

---
+++ src/Highlightable.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Highlightable extends Component {
+  state = {
+    highlighted: false
+  };
+
+  handleClick() {
+    this.setState(function(state, props) {
+      return { highlighted: this.state.highlighted ? false : true };
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className={`pa1 br2 f3 mt3 pointer ${
+          this.state.highlighted ? "bg-yellow" : ""
+        }`}
+        onClick={() => this.handleClick()}
+      >
+        {this.props.text}
+      </div>
+    );
+  }
+}
+
+export default Highlightable;
--- src/Instruction.js
+++ src/Instruction.js
@@ -1,27 +1,9 @@
 import React, { Component } from "react";
+import Highlightable from "./Highlightable";
 
 class Instruction extends Component {
-  state = {
-    highlighted: false
-  };
-
-  handleClick() {
-    this.setState(function(state, props) {
-      return { highlighted: this.state.highlighted ? false : true };
-    });
-  }
-
   render() {
-    return (
-      <div
-        className={`pa1 br2 f3 mt3 pointer ${
-          this.state.highlighted ? "bg-yellow" : ""
-        }`}
-        onClick={() => this.handleClick()}
-      >
-        {this.props.text}
-      </div>
-    );
+    return <Highlightable text={this.props.text} />;
   }
 }
 
