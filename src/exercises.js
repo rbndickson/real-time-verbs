1f351207d9876f5f348125a39c2903f29805fad6
Robin Dickson <robindickson@gmail.com>
2018-06-14 23:57:46 +0700

    feat: Add data for present simple tense exercise

--- src/exercises.js
+++ src/exercises.js
@@ -20,5 +20,48 @@ export const exercises = {
       "in",
       "ought"
     ]
+  },
+  presentSimple: {
+    instruction: "Make a present simple tense sentence.",
+    example: [
+      { question: ["I", "play", "O"], solution: "I play soccer" },
+      { question: ["He/She/It", "play", "O"], solution: "He plays tennis" },
+      {
+        question: ["We/They/You", "play", "O"],
+        solution: "We/They/You play basketball"
+      },
+      { question: ["I", "play", "X"], solution: "I don't play ~" },
+      {
+        question: ["He/She/It", "play", "X"],
+        solution: "He/She/It doesn't play ~"
+      },
+      {
+        question: ["He/She/It", "play", "X"],
+        solution: "He/She/It doesn't play ~"
+      },
+      { question: ["You", "play", "?"], solution: "Do you play ~ ?" },
+      {
+        question: ["He/She/It", "play", "?"],
+        solution: "Does he/she/it play ~ ?"
+      },
+      {
+        question: ["They/You", "play", "?"],
+        solution: "Do they/you play ~ ?"
+      }
+    ],
+    omitPronouns: [],
+    omitVerbs: [
+      "will",
+      "would",
+      "could",
+      "may",
+      "should",
+      "must",
+      "might",
+      "going",
+      "let_'s",
+      "in",
+      "ought"
+    ]
   }
 };
