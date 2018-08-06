648ecef8c02a9e4dad52896ed53c63aa669a1845
Robin Dickson <robindickson@gmail.com>
2018-08-06 09:49:34 +0700

    feat: Re-style question
    - Remove background color
    - Add different color underlines
    
    So that they the colors can be used in explanations during lessons.

--- src/Question.js
+++ src/Question.js
@@ -3,25 +3,25 @@ import { typeToEmoji } from "./utils/helpers";
 import Hideable from "./Hideable";
 
 const Question = props => (
-  <div className="tc mt5 mb4">
+  <div className="tc mt5 mb4 f1-ns f3">
     <Hideable
       socket={props.socket}
       id="questionPronoun"
-      className="dib w-20 f1-ns f3 mh3 pv2 bg-light-pink"
+      className="dib w-20 bb b--purple mh3 pv2"
     >
       <span>{props.question.pronoun}</span>
     </Hideable>
     <Hideable
       socket={props.socket}
       id="questionVerb"
-      className="dib w-40 f1-ns f3 mh3 pv2 bg-light-pink"
+      className="dib w-40 bb b--hot-pink mh3 pv2"
     >
       <span>{props.question.verb.base}</span>
     </Hideable>
     <Hideable
       socket={props.socket}
       id="questionType"
-      className="dib w-10 f1-ns f3  mh3 pv2 bg-light-pink"
+      className="dib w-10 bb b--orange mh3 pv2"
     >
       <div>{typeToEmoji(props.question.type)}</div>
     </Hideable>
